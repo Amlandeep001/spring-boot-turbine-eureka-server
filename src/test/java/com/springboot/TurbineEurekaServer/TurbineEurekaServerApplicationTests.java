7baaaa4f55467aff1e616391461e5340fae8adc0
Amlan Deep Nandi <Amlandeep001@users.noreply.github.com>
2022-05-08 17:53:28 +0530

    Turbine Eureka Server Initial Commit
    Spring Boot Turbine Eureka Server Initial Commit.

---
+++ src/test/java/com/springboot/TurbineEurekaServer/TurbineEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.TurbineEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
